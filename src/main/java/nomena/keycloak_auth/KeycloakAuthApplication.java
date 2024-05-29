0b93ad6b1b6d0a59c9930bc807b35e85610ccba6
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-29 09:27:15 +0300

    setup project

---
+++ src/main/java/nomena/keycloak_auth/KeycloakAuthApplication.java
@@ -0,0 +1,13 @@
+package nomena.keycloak_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nomena/keycloak_auth/KeycloakAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package nomena.keycloak_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
